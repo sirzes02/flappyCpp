1e9dd0a395795e41683bdd6111bd9f80d7dd690a
sirzes02 <sirzes02@gmail.com>
2024-06-11 22:05:32 -0500

    Initial Commit

---
+++ Flappy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Flappy
+//
+//  Created by Santiago Varela on 11/06/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
