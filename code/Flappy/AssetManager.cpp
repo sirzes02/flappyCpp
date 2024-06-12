e815f44f3398d4fb9bdcfd4be076266939cc5112
sirzes02 <sirzes02@gmail.com>
2024-06-11 23:53:42 -0500

    Load manager

---
+++ code/Flappy/AssetManager.cpp
@@ -0,0 +1,34 @@
+//
+//  AssetManager.cpp
+//  Flappy
+//
+//  Created by Santiago Varela on 11/06/24.
+//
+
+#include "AssetManager.hpp"
+
+namespace Sonar {
+void AssetManager::LoadTexture(std::string name, std::string fileName) {
+    sf::Texture tex;
+    
+    if (tex.loadFromFile(fileName)) {
+        this->_textures[name] = tex;
+    }
+}
+
+sf::Texture &AssetManager::GetTexture(std::string name) {
+    return this->_textures.at(name);
+}
+
+void AssetManager::LoadFont(std::string name, std::string fileName) {
+    sf::Font font;
+    
+    if (font.loadFromFile(fileName)) {
+        this->_fonts[name] = font;
+    }
+}
+
+sf::Font &AssetManager::GetFont(std::string name) {
+    return this->_fonts.at(name);
+}
+}
