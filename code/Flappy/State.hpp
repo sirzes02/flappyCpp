4f77ef5b4566bf46d8840f6f1928edb99f59c4bc
sirzes02 <sirzes02@gmail.com>
2024-06-12 00:24:44 -0500

    Game

--- code/Flappy/State.hpp
+++ code/Flappy/State.hpp
@@ -13,7 +13,7 @@ public:
     virtual void Init() = 0;
     
     virtual void HandleInput() = 0;
-    virtual void update() = 0;
+    virtual void Update(float dt) = 0;
     virtual void Draw(float dt) = 0;
     
     virtual void Pause() {}
--- code/Flappy/main.cpp
+++ code/Flappy/main.cpp
@@ -6,7 +6,10 @@
 //
 
 #include <iostream>
+#include "Game.hpp"
 
 int main() {
+    Sonar::Game(800, 600, "Test started");
+
     return EXIT_SUCCESS;
 }
